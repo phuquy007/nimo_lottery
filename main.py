ae3ea8abf1d2d989b5ebd06bace79f911b992d0a
Ryan Tran <tranp6@student.douglascollege.ca>
2021-08-17 23:33:37 -0700

    .\byType.py

--- main.py
+++ main.py
@@ -15,13 +15,14 @@ def printList(collection):
 
 baseBoxes = []
 for type in types:
-    baseBoxes.append({"type": type, "percentage": boxCollection.count_documents({"type": type})/totalCount })
+    # baseBoxes[type] = {"percentage": boxCollection.count_documents({"type": type})/totalCount }
+    baseBoxes.append({"type": type, "percentage": boxCollection.count_documents({"type": type})/totalCount * 100})
 
 print("Total Count: " + str(totalCount))
 for box in baseBoxes:
-    print("Box Type: " + box["type"] + " - Percentage: " + str(round(box["percentage"], 4)))
+    print("Box Type: " + box["type"] + " - Percentage: " + str(round(box["percentage"], 2)))
 
-#Get the newest 100
+#Get 100 newest boxes
 curTotal = 100
 newestCollection =list(boxCollection.find().limit(100).sort("time", -1))
 curBoxes = []
@@ -32,4 +33,17 @@ for box in curBoxes:
     print("Current Box "+ box["type"] + " - " + str(box["percentage"]))
 
 
+# get the difference between the base and the current percentage
 predicts = []
+for type in types:
+    # get the newest of each type
+    lastestBox = list(boxCollection.find({}).sort("time",-1).limit(1))
+    lastestBoxByType = list(boxCollection.find({"type": type}).sort("time",-1).limit(1))
+    basePercent = next((x for x in baseBoxes if x["type"] == type), None)["percentage"]
+    curPercent = next((x for x in curBoxes if x["type"] == type), None)["percentage"]
+    baseOccur = 100 / basePercent
+
+    predicts.append({"type": type, "round":lastestBoxByType[0]["round"], "percentDiff": (basePercent - curPercent), "baseOccur": baseOccur, "distanceDiff": int(lastestBox[0]["round"]) - int(lastestBoxByType[0]["round"]) - baseOccur, "time": lastestBoxByType[0]["time"]})
+    
+for predict in predicts:
+    print(predict)
